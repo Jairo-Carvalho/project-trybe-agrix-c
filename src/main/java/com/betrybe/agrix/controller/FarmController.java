40ab21b6358424289a94200fb728f2ffb8a2f96f
jairo <jairocdourado@gmail.com>
2023-11-24 16:16:21 -0300

    req 4 - pra que um requisito tão difícil??

--- src/main/java/com/betrybe/agrix/controller/FarmController.java
+++ src/main/java/com/betrybe/agrix/controller/FarmController.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -79,6 +80,7 @@ public class FarmController {
    * @return the farm by id
    */
   @GetMapping("/{id}")
+  @Secured({"USER", "MANAGER", "ADMIN"})
   public ResponseEntity<Object> getFarmById(@PathVariable Integer id) {
     Optional<Farm> farm = farmService.getFarmById(id);
 
